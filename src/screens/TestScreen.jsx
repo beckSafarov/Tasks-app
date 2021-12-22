48827912634b1503e86ed2296932fa91b6078079
Begzod Safarov <begzod.safarov07@gmail.com>
2021-12-22 11:34:14 +0800

    cleaned junk in the testscreen

--- src/screens/TestScreen.jsx
+++ src/screens/TestScreen.jsx
@@ -1,110 +1,33 @@
-import { useState, useEffect, useContext } from 'react'
-import { Box } from '@chakra-ui/react'
-import SearchTask from '../components/SearchTask'
-import ReactDatePicker from 'react-datepicker'
+import { useState } from 'react'
 import dayjs from 'dayjs'
 import localizedFormat from 'dayjs/plugin/localizedFormat'
 import relativeTime from 'dayjs/plugin/relativeTime'
-import { taskSchema, TasksContext } from '../Context/TasksContext'
-import { AppContext } from '../Context/AppContext'
-import usePageData from '../hooks/usePageData'
-import SkeletonStack from '../components/SkeletonStack'
 dayjs.extend(localizedFormat)
 dayjs.extend(relativeTime)
 
 const TestScreen = () => {
   const [foo, setFoo] = useState(false)
-  const [date, setDate] = useState(new Date())
-  const [tasks, setTasks] = useState([])
-  const {
-    loading: updateLoading,
-    add: addTaskToDB,
-    data: updatedData,
-    error,
-  } = useContext(TasksContext)
-  const {
-    loading: appLoading,
-    getData,
-    data,
-    error: appErr,
-  } = useContext(AppContext)
 
-  // const pageData = usePageData({
-  //   pathname: 'http://localhost:3000/tag/mcd',
-  // })
-  // console.log(pageData)
-
-  const loading = updateLoading || appLoading
-
-  const toggleFoo = async (e) => {
-    await getData()
-  }
-
-  const addTask = async () => {
-    await addTaskToDB({ ...taskSchema, id: 'sixth_data', tag: 'tag_2' })
-  }
-
-  const format = {
-    daysSince: (date = '2021-10-10') => dayjs(date).diff(new Date(), 'days'),
-  }
+  const toggleFoo = (e) => {}
 
   return (
-    <>
-      <div>
-        {loading ? 'Loading...' : ''}
-        {/* {data && console.log(data)}
-        {error && console.log(error)} */}
-        {/* <SkeletonStack /> */}
-      </div>
+    <div style={{ padding: '50px' }}>
+      <h2>
+        <strong>Foo value: </strong>
+        <span style={{ color: foo ? 'green' : 'red' }}>
+          {foo ? 'true' : 'false'}
+        </span>
+      </h2>
       <br />
-      <div
-        style={{
-          width: '100%',
-          padding: '50px',
-        }}
+      <button
+        className='jobona'
+        name='my name is d'
+        style={{ border: '1px solid #ccc' }}
+        onClick={toggleFoo}
       >
-        <Box>
-          <SearchTask onSubmit={(v) => console.log(v)} />
-        </Box>
-        {/* <AddTagModal show={show} close={close} onSubmit={proceed} /> */}
-        <h2>
-          <strong>Foo value: </strong>
-          <span style={{ color: foo ? 'green' : 'red' }}>
-            {foo ? 'true' : 'false'}
-          </span>
-        </h2>
-        <br />
-        <button
-          className='jobona'
-          name='my name is d'
-          style={{ border: '1px solid #ccc' }}
-          onClick={toggleFoo}
-        >
-          Click to toggle foo
-        </button>
-        <button className='btn' onClick={addTask}>
-          Add Task
-        </button>
-
-        <br />
-        <p>Past Date: {format.daysSince('2021-10-18')}</p>
-        <br />
-
-        {/* <AccountModal show /> */}
-
-        <Box mt='30px'>
-          <ReactDatePicker
-            selected={date}
-            onChange={(d) => setDate(d)}
-            timeInputLabel='Time:'
-            dateFormat='MM/dd/yyyy h:mm aa'
-            shouldCloseOnSelect={false}
-            showTimeInput
-          />
-          <br />
-        </Box>
-      </div>
-    </>
+        Click to toggle foo
+      </button>
+    </div>
   )
 }
 
